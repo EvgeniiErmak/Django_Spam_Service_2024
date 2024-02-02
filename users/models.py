fade927cddced3277a156c3577b2cc095ae58d0f
Евгений Ермак <144560375+EvgeniiErmak@users.noreply.github.com>
2024-02-03 01:49:01 +0300

    После созвона

--- users/models.py
+++ users/models.py
@@ -6,15 +6,14 @@ NULLABLE = {'blank': True, 'null': True}
 
 
 class User(AbstractUser):
-    username = models.CharField(max_length=15, verbose_name='Имя пользователя', **NULLABLE)
+    username = models.CharField(max_length=30, verbose_name='Имя пользователя', **NULLABLE)
+    phone = models.CharField(max_length=30, verbose_name='телефон', **NULLABLE)
+    country = models.CharField(max_length=50, verbose_name='страна', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
     email = models.EmailField(unique=True, verbose_name='почта')
 
     is_active = models.BooleanField(default=False)
     user_token = models.CharField(max_length=255, blank=True, null=True, verbose_name='token')
-
-    phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
-    country = models.CharField(max_length=15, verbose_name='страна', **NULLABLE)
-    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ['username']
